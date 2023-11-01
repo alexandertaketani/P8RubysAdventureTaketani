5ec2864a92d29f2c6e0158cdfe89149e2f394ae4
Alexander T <719277@student.dusd.net>
2023-11-01 15:24:48 -0700

    Added tilemaps
    created tilemaps and decorated

--- RubysAdventureTaketani/Assets/Sprites/Scripts/RubyContoller.cs
+++ RubysAdventureTaketani/Assets/Sprites/Scripts/RubyContoller.cs
@@ -7,10 +7,8 @@ public class RubyContoller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
@@ -18,8 +16,8 @@ public class RubyContoller : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.01f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
