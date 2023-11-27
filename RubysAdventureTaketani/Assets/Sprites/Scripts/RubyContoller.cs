9f28986b72ed2b7d68b6ce80436e0c2ce60379e6
Alexander T <719277@student.dusd.net>
2023-11-26 23:25:35 -0800

    animations
    animations added

--- RubysAdventureTaketani/Assets/Sprites/Scripts/RubyContoller.cs
+++ RubysAdventureTaketani/Assets/Sprites/Scripts/RubyContoller.cs
@@ -3,22 +3,29 @@ using UnityEngine;
 public class RubyContoller : MonoBehaviour
 {
     public float speed = 3.0f;
-
+    
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
 
-    public int health { get { return currentHealth; } }
+    public int health { get { return currentHealth; }}
     int currentHealth;
-
+    
     bool isInvincible;
     float invincibleTimer;
-
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+    
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+    
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
         currentHealth = maxHealth;
     }
 
@@ -27,7 +34,19 @@ public class RubyContoller : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-
+        
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+        
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -35,13 +54,13 @@ public class RubyContoller : MonoBehaviour
                 isInvincible = false;
         }
     }
-
+    
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
-        
+
         rigidbody2d.MovePosition(position);
     }
 
@@ -49,14 +68,16 @@ public class RubyContoller : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
-
-            isInvincible - true;
-            invincibleTimer - timeInvincible;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
         }
-
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
+
