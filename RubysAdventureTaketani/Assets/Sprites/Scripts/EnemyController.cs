75c787d76edbf6c20bb38c0403377ce69e9ec030
Alexander T <719277@student.dusd.net>
2023-12-01 15:26:36 -0800

    ui added
    cool

--- RubysAdventureTaketani/Assets/Sprites/Scripts/EnemyController.cs
+++ RubysAdventureTaketani/Assets/Sprites/Scripts/EnemyController.cs
@@ -8,13 +8,16 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
     bool broken = true;
-    
+
     Animator animator;
-    
+
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -24,12 +27,12 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
-        
-    if(!broken)
-    {
-        return;
-    }
-        
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -38,16 +41,17 @@ public class EnemyController : MonoBehaviour
             timer = changeTime;
         }
     }
-    
+
     void FixedUpdate()
     {
-        if(!broken)
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
         {
             return;
         }
-        
+
         Vector2 position = rigidbody2D.position;
-        
+
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
@@ -60,25 +64,28 @@ public class EnemyController : MonoBehaviour
             animator.SetFloat("Move X", direction);
             animator.SetFloat("Move Y", 0);
         }
-        
+
         rigidbody2D.MovePosition(position);
     }
-    
+
     void OnCollisionEnter2D(Collision2D other)
     {
-        RubyContoller player = other.collider.GetComponent<RubyContoller >();
+        RubyContoller player = other.gameObject.GetComponent<RubyContoller>();
 
         if (player != null)
         {
             player.ChangeHealth(-1);
         }
     }
-    
+
+    //Public because we want to call it from elsewhere like the projectile script
     public void Fix()
     {
         broken = false;
         rigidbody2D.simulated = false;
-        
+        //optional if you added the fixed animation
         animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 }
